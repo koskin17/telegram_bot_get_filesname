5f82d9211e4a572ee6b952d2475eaefe84faae85
Константин Зейкин <kostia.zeykin@gmail.com>
2026-03-03 13:18:10 +0200

    add all function in main.app

--- app/main.py
+++ app/main.py
@@ -4,21 +4,38 @@ from app.topic_service import list_topics
 from app.file_service import get_files_from_topic
 
 client = get_client()
+chat_link = "https://t.me/+CXM7VjXkpeMzMmEy"    # My group
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
     await event.respond(
-        "Я user-bor для получения списка файлов по темам.\N"
+        "Я user-bor для получения списка файлов по темам.\n"
         "Отправь мне /topics чтобы увидеть список тем."
         )
     
-@client.on(event.NewMessage(pattern="/topics"))
+@client.on(events.NewMessage(pattern="/topics"))
 async def topics(event):
-    chat_link = "https://t.me/+CXM7VjXkpeMzMmEy"    # My group
     topics = await list_topics(client, chat_link)
 
     msg = "Список тем:\n"
     for i, t in enumerate(topics, 1):
-        msg += f"{i}. {t.title}\n}"
+        msg += f"{i}. {t.title}\n"
     await event.respond(msg)
-    
+
+@client.on(events.NewMessage(patterm="/getfiles"))
+async def get_files(event):
+    topic_number = int(input("Введите номер темы: "))
+
+    file_list = await get_files_from_topic(client, chat_link, topic_number)
+
+    with open("file_list.txt", "w", encoding="utf-8") as f:
+        for file in file_list:
+            f.write(f"{file}\n")
+
+    await event.respond("Список файлов сохранён в files_list.txt")
+
+
+if __name__ == "__main__":
+    if client.start():
+        print("Бот запущен")
+        client.run_until_disconnected() 
